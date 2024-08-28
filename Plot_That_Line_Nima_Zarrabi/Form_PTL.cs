df1d8e51355e86699eaba3e30a97cd1cf70383ce
Nima Amir Aram Zarrabi <pu78ifh@eduvaud.ch>
2024-08-28 16:09:36 +0200

    initial commit, planning and JDT done, WinForms untouched

---
+++ Plot_That_Line_Nima_Zarrabi/Form_PTL.cs
@@ -0,0 +1,15 @@
+namespace Plot_That_Line_Nima_Zarrabi
+{
+    public partial class Form_PTL : Form
+    {
+        public Form_PTL()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
