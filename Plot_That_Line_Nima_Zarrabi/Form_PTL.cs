e97d3a3b533213d9479f9944eca70eb1a233c46c
Nima Zarrabi <pu78ifh@eduvaud.ch>
2024-09-20 08:25:44 +0200

    multi-values still not functional, need to make class to avoid out of range error

--- Plot_That_Line_Nima_Zarrabi/Form_PTL.cs
+++ Plot_That_Line_Nima_Zarrabi/Form_PTL.cs
@@ -3,7 +3,9 @@ namespace Plot_That_Line_Nima_Zarrabi
     using Microsoft.VisualBasic.FileIO;
     using ScottPlot;
     using ScottPlot.Colormaps;
+    using ScottPlot.PlotStyles;
     using ScottPlot.Plottables;
+    using System.Collections.Generic;
     using static System.Runtime.InteropServices.JavaScript.JSType;
 
     public partial class Form_PTL : Form
@@ -15,10 +17,11 @@ namespace Plot_That_Line_Nima_Zarrabi
            
             List<string> paths =
             [
-                //@"C:\Users\pu78ifh\Desktop\P_Plot_That_Line\Data\Filecoin.csv",
-                @"C:\Users\pu78ifh\Desktop\P_Plot_That_Line\Data\Bitcoin_SV.csv",
-                //@"C:\Users\pu78ifh\Desktop\P_Plot_That_Line\Data\Filecoin.csv",
+                @"C:\Users\pu78ifh\Desktop\P_Plot_That_Line\Data\Filecoin.csv",
+                //@"C:\Users\pu78ifh\Desktop\P_Plot_That_Line\Data\Bitcoin_Gold.csv",
+                @"C:\Users\pu78ifh\Desktop\P_Plot_That_Line\Data\Fantom.csv",
             ];
+            TextFieldParser parsedcsv;
 
 
             // data list initialization 
@@ -31,56 +34,74 @@ namespace Plot_That_Line_Nima_Zarrabi
             List<string> currencies = new List<string>();
             List<DateTime> datetime = new List<DateTime>();
 
+            // data initialization 
+            string[] fields;
+            string date;
+            string open;
+            string high;
+            string low;
+            string close;
+            string volume;
+            string currency;
+
 
             foreach (string csv_path in paths)
             {
 
-                using (TextFieldParser csvParser = new TextFieldParser(csv_path))
+                parsedcsv = new TextFieldParser(csv_path);
+                // Set CSV parser
+                parsedcsv.CommentTokens = new string[] { "#" };
+                parsedcsv.SetDelimiters(new string[] { "," });
+                parsedcsv.HasFieldsEnclosedInQuotes = true;
+
+                // Skip the row with the column names
+                parsedcsv.ReadLine();
+
+                while (!parsedcsv.EndOfData)
                 {
-                    // Set CSV parser
-                    csvParser.CommentTokens = new string[] { "#" };
-                    csvParser.SetDelimiters(new string[] { "," });
-                    csvParser.HasFieldsEnclosedInQuotes = true;
-
-                    // Skip the row with the column names
-                    csvParser.ReadLine();
-
-                    while (!csvParser.EndOfData)
-                    {
-                        // Read current line fields, pointer moves to the next line.
-                        string[] fields = csvParser.ReadFields();
-                        string date = fields[0];
-                        string open = fields[1];
-                        string high = fields[2];
-                        string low = fields[3];
-                        string close = fields[4];
-                        string volume = fields[5];
-                        string currency = fields[6];
+                    // Read current line fields, pointer moves to the next line.
+                    fields = parsedcsv.ReadFields();
+                    date = fields[0];
+                    open = fields[1];
+                    high = fields[2];
+                    low = fields[3];
+                    close = fields[4];
+                    volume = fields[5];
+                    currency = fields[6];
 
 
-                        // Add to the lists
+                    // Add to the lists
 
-                        datetime.Add(DateTime.Parse(date));
-                        dates.Add(date);
-                        opens.Add(double.Parse(open)); // Convert open to double
-                        highs.Add(double.Parse(high)); // Convert high to double
-                        lows.Add(double.Parse(low)); // Convert low to double
-                        closes.Add(double.Parse(close)); // Convert close to double
-                        volumes.Add(double.Parse(volume)); // Convert volume to double
-                        currencies.Add(currency);
+                    datetime.Add(DateTime.Parse(date));
+                    dates.Add(date);
+                    opens.Add(double.Parse(open)); // Convert open to double
+                    highs.Add(double.Parse(high)); // Convert high to double
+                    lows.Add(double.Parse(low)); // Convert low to double
+                    closes.Add(double.Parse(close)); // Convert close to double
+                    volumes.Add(double.Parse(volume)); // Convert volume to double
+                    currencies.Add(currency);
 
-                    }
                 }
                 // use LINQ and DateTime.ToOADate() to convert DateTime[] to double[]
                 //double[] doubledatetime = datetime.Select(x => x.ToOADate()).ToArray();
 
                 // Now plot the data
+                formsPlot1.Plot.Add.ScatterLine(datetime, closes);
 
                 formsPlot1.Plot.YLabel("Price per unit (in " + currencies[0] + ")");
                 // tell the plot to display dates on the bottom axis
                 formsPlot1.Plot.Axes.DateTimeTicksBottom();
 
                 formsPlot1.Refresh();
+
+                // OUT OF RANGE PROBLEM, NEED TO MAKE CLASSES
+                datetime.Clear();
+                dates.Clear();
+                opens.Clear();
+                highs.Clear();
+                lows.Clear();
+                volumes.Clear();
+                currencies.Clear();
             }
         }
 
--- Plot_That_Line_Nima_Zarrabi/obj/Debug/net8.0-windows/Plot_That_Line_Nima_Zarrabi.AssemblyInfo.cs
+++ Plot_That_Line_Nima_Zarrabi/obj/Debug/net8.0-windows/Plot_That_Line_Nima_Zarrabi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Plot_That_Line_Nima_Zarrabi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8725b3c6eed2bde2e9d4e484fb36aa85fbf911e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eada706e2cbffbbb49902cde556a7508b710dc31")]
 [assembly: System.Reflection.AssemblyProductAttribute("Plot_That_Line_Nima_Zarrabi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Plot_That_Line_Nima_Zarrabi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
